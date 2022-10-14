03f9a88347ae54e059751eca2dee71c854a79b5e
sagaeva <sagaeva@gmail.com>
2022-10-15 00:12:31 +0200

    added security classes

---
+++ src/main/java/be/scrumdilicious/blogOpdracht/services/UserDetailsServ.java
@@ -0,0 +1,42 @@
+package be.scrumdilicious.blogOpdracht.services;
+
+import be.scrumdilicious.blogOpdracht.data.User;
+import be.scrumdilicious.blogOpdracht.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServ implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServ(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByUsername(username);
+        if (user.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new be.scrumdilicious.blogOpdracht.security.UserDetails(user.get());
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
