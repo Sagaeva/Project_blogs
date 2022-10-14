c0be52712f32d45136279e1844076faa7fc5b429
sagaeva <sagaeva@gmail.com>
2022-10-14 18:12:40 +0200

    Mezelf

---
+++ src/main/java/be/scrumdilicious/blogOpdracht/data/ERole.java
@@ -0,0 +1,7 @@
+package be.scrumdilicious.blogOpdracht.data;
+
+public enum ERole {
+
+    ROLE_GEBRUIKERS,
+    ROLE_AUTHOR
+}
---
+++ src/main/java/be/scrumdilicious/blogOpdracht/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package be.scrumdilicious.blogOpdracht.repository;
+
+import be.scrumdilicious.blogOpdracht.data.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUserName(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+
+
+}
